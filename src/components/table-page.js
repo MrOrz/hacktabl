33f8cebd755a62fb3f644114bfcc4d362918f441
MrOrz <johnsonliang7@gmail.com>
2015-12-23 23:24:41 +0800

    [table-page] Card view

--- src/components/table-page.js
+++ src/components/table-page.js
@@ -12,9 +12,11 @@ class Cell extends React.Component {
   render() {
     return (
       <div className={styles.cell}>
-        <pre>
-          {JSON.stringify(this.props, null, '  ')}
-        </pre>
+        <div className={styles.cellCard}>
+          <pre>
+            {JSON.stringify(this.props, null, '  ')}
+          </pre>
+        </div>
       </div>
     )
   }
@@ -35,36 +37,29 @@ class TablePage extends React.Component {
       return <div>Loading...</div>
     }
 
-    let lastDividerText = null
-    let titlePart = null
+    let lastAncestorText = null
+
     for(let row of iterateRows(this.props.currentTable.data.table.rows)) {
       let rowTitle = concatAllParagraphs(row.headers[row.headers.length-1])
-      let dividerText = row.headers.slice(0, -1).map(
+      let ancestorText = row.headers.slice(0, -1).map(
         paragraphs => concatAllParagraphs(paragraphs)).join('／');
-
-      if(row.headers.length > 1 && dividerText !== lastDividerText) {
-        titlePart = (
-          <h6 className={styles.divider}>
-            {dividerText}
-          </h6>
-        )
-
-        lastDividerText = dividerText
-      }else{
-        titlePart = null
-      }
-
+      let ancestorElem = ancestorText ? (
+          <small>{ancestorText}／</small>
+        ) : ''
       let cellElems = row.cells.map((cellProps, idx) => <Cell {...cellProps} key={idx} />)
 
+      let isDivider = ancestorText === '' || ancestorText !== lastAncestorText
+
       rowElems.push(
-        <div className={styles.row} key={`${dividerText}-${rowTitle}`}>
-          {titlePart}
-          <h5>{rowTitle}</h5>
+        <div className={`${styles.row} ${isDivider ? styles.isDivider : ''}`} key={`${ancestorText}-${rowTitle}`}>
+          <h5 className={styles.rowHeader}>{ancestorElem}{rowTitle}</h5>
           <div className={styles.cellContainer}>
             {cellElems}
           </div>
         </div>
       )
+
+      lastAncestorText = ancestorText
     }
     return (
       <div>
