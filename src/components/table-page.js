beb092ccf41fc7dccbb5836e3f11e5b95b4042be
MrOrz <johnsonliang7@gmail.com>
2015-12-24 11:19:09 +0800

    [TablePage] Make row header fix on screen

--- src/components/table-page.js
+++ src/components/table-page.js
@@ -24,6 +24,28 @@ class Cell extends React.Component {
 
 Cell.propTypes = TABLE_CELL_PROPS
 
+class RowHeader extends React.Component {
+  render() {
+    let ancestorElem = this.props.ancestorText ? (
+      <small>{this.props.ancestorText}／</small>
+    ) : ''
+
+    return (
+      <h5 className={styles.rowHeader} style={{
+        transform: `translate3d(${this.props.scrollLeft}px,0,0)`
+      }}>{ancestorElem}{this.props.rowTitle}</h5>
+    )
+  }
+}
+
+RowHeader.propTypes = {
+  ancestorText: PropTypes.string,
+  rowTitle: PropTypes.string,
+  scrollLeft: PropTypes.number
+}
+
+RowHeader = connect(state => state.ui)(RowHeader)
+
 class TablePage extends React.Component {
   componentDidMount() {
     let tableId = this.props.params.tableId;
@@ -43,16 +65,14 @@ class TablePage extends React.Component {
       let rowTitle = concatAllParagraphs(row.headers[row.headers.length-1])
       let ancestorText = row.headers.slice(0, -1).map(
         paragraphs => concatAllParagraphs(paragraphs)).join('／');
-      let ancestorElem = ancestorText ? (
-          <small>{ancestorText}／</small>
-        ) : ''
+
       let cellElems = row.cells.map((cellProps, idx) => <Cell {...cellProps} key={idx} />)
 
       let isDivider = ancestorText === '' || ancestorText !== lastAncestorText
 
       rowElems.push(
         <div className={`${styles.row} ${isDivider ? styles.isDivider : ''}`} key={`${ancestorText}-${rowTitle}`}>
-          <h5 className={styles.rowHeader}>{ancestorElem}{rowTitle}</h5>
+          <RowHeader ancestorText={ancestorText} rowTitle={rowTitle} />
           <div className={styles.cellContainer}>
             {cellElems}
           </div>
