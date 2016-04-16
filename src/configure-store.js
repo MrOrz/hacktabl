5a262e1f8afca249813dfbcd1c75f596e2fd097e
MrOrz <johnsonliang7@gmail.com>
2016-04-16 17:57:58 +0800

    [configure-store] Use redux-devtool chrome extension instead of redux-logger

--- src/configure-store.js
+++ src/configure-store.js
@@ -1,23 +1,17 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {SET_UI} from './actions'
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import rootReducer from './reducers';
-
 
 var createStoreWithMiddleware
 
 if(DEBUG){
-  let loggerMiddleware = createLogger({
-    // Don't log SET_UI actions, since they are too often
-    //
-    predicate: (getState, action) => action.type !== SET_UI
-  })
-  createStoreWithMiddleware = applyMiddleware(
-  thunk, loggerMiddleware)(createStore);
+  createStoreWithMiddleware = compose(
+    applyMiddleware(thunk),
+     window.devToolsExtension ? window.devToolsExtension() : f => f
+  )(createStore);
 }else{
-  createStoreWithMiddleware = applyMiddleware(
-  thunk)(createStore);
+  createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 }
 
 export default function configureStore(initialState) {
